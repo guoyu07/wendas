c060cb652d2ac40348404f28a46f8203329c3123
dichanger <dichanger@126.com>
2016-05-23 16:57:05 +0800

    1

--- Index/Conf/db.php
+++ Index/Conf/db.php
@@ -1,13 +1,13 @@
 <?php
 return array (
   'DB_TYPE' => 'mysql',
-  'DB_HOST' => 'localhost',
+  'DB_HOST' => MYSQL_CONNECTION,
   'DB_PORT' => '3306',
 //  'DB_NAME' => 'dongwthb_xiaohua',
-  'DB_NAME' => 'xiaohua',
+  'DB_NAME' => MYSQL_INSTANCE_NAME,
 //  'DB_USER' => 'dongwthb',
-  'DB_USER' => 'root',
-  'DB_PWD' => 'root',
+  'DB_USER' => MYSQL_USERNAME,
+  'DB_PWD' =>  MYSQL_PASSWORD,
   'DB_PREFIX' => 'xh_',
 );
 ?>
